e941cdb6daf3fca22d1a94a024e3122dd358e696
Mencarelli-Simone <smen851@aucklanduni.ac.nz>
2025-05-20 18:30:48 +1200

    added region to the query

--- SRSdatabaseQuery.sql
+++ SRSdatabaseQuery.sql
@@ -1,4 +1,3 @@
--- Compatible with msAccess and BRSIS spaceQRY, change the adm name to get the table (you need the srsxxx.mdb files)
 SELECT  DISTINCT
     com_el.ntc_id,
     com_el.tgt_ntc_id,
@@ -34,7 +33,10 @@ SELECT  DISTINCT
 
     carrier_fr.freq_carr,
     carrier_fr.seq_emiss,
-    carrier_fr.seq_no
+    carrier_fr.seq_no,
+
+    srv_area.ctry,
+    srv_area.f_excl_api
 
 FROM
     (
@@ -42,28 +44,32 @@ FROM
             (
                 (
                     (
-                        SELECT * FROM com_el
-                        WHERE adm = 'D'
-                          AND (
-                                (ntc_type IN ('G', 'N') AND ntf_rsn IN ('A', 'C', 'N', 'U', 'D'))
-                                OR (ntf_rsn = 'C' AND ntc_type IN ('S', 'T'))
-                              )
-                    ) AS com_el
-                    LEFT JOIN orbit ON com_el.ntc_id = orbit.ntc_id
+                        (
+                            SELECT * FROM com_el
+                            WHERE adm = 'HOL'
+                            AND (
+                                    (ntc_type IN ('G', 'N') AND ntf_rsn IN ('A', 'C', 'N', 'U', 'D'))
+                                    OR (ntf_rsn = 'C' AND ntc_type IN ('S', 'T'))
+                                )
+                        ) AS com_el
+                        LEFT JOIN orbit ON com_el.ntc_id = orbit.ntc_id
+                    )
+                    LEFT JOIN s_beam ON com_el.ntc_id = s_beam.ntc_id
                 )
-                LEFT JOIN s_beam ON com_el.ntc_id = s_beam.ntc_id
+                LEFT JOIN (
+                    SELECT * FROM grp
+                    WHERE
+                        (freq_max > 401.825 AND freq_min < 402)
+                        OR (freq_max > 2055 AND freq_min < 2075)
+                        OR (freq_max > 2202 AND freq_min < 2239)
+                ) AS grp ON s_beam.ntc_id = grp.ntc_id AND s_beam.beam_name = grp.beam_name AND s_beam.emi_rcp = grp.emi_rcp
             )
-            LEFT JOIN (
-                SELECT * FROM grp
-                WHERE
-                    (freq_max > 401.825 AND freq_min < 402)
-                    OR (freq_max > 2055 AND freq_min < 2075)
-                    OR (freq_max > 2202 AND freq_min < 2239)
-            ) AS grp ON s_beam.ntc_id = grp.ntc_id AND s_beam.beam_name = grp.beam_name AND s_beam.emi_rcp = grp.emi_rcp
+            LEFT JOIN srv_area ON grp.grp_id = srv_area.grp_id
         )
         LEFT JOIN emiss ON grp.grp_id = emiss.grp_id
     )
     LEFT JOIN carrier_fr ON emiss.grp_id = carrier_fr.grp_id AND emiss.seq_no = carrier_fr.seq_emiss
+
 WHERE
                     (grp.freq_max > 401.825 AND grp.freq_min < 402)
                     OR (grp.freq_max > 2055 AND grp.freq_min < 2075)
